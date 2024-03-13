89b835b35728681b666718111441484ef9c981bf
eli-soto <142184979+eli-soto@users.noreply.github.com>
2024-03-12 20:49:16 -0400

    creating generator function

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-// Assignment code here
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// Generator Function
+function getPassword(){
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var number = "0123456789"
+  var specialchar = "!@#$%^&*()-=+<>?/|':[]{}"
+  var random = "";
+  var key ="";
+
+  //Criteria Choices
+  var keyLength = confirm("Please choose from the following criteria for your password:.");
+  var uppercase = confirm("Your password will include uppercase letters. To accept click ok to decline click cancel.");
+  var lowercase = confirm("Your password will include lowercase letters. To accept click ok to decline click cancel.");    
+  var numbers = confirm("Your password will include numbers. To accept click ok to decline click cancel.");
+  var symbols = confirm("Your password will include special characters. To accept click ok to decline click cancel.");
+  var keyLength = prompt(
+      "Please select a password length. Passwords must be 8 to 28 characters.");
+    
+
+
+
+
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
